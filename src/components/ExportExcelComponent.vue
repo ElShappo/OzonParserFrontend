83cebf11f7af5a53d9ba937dbea56e98f9972c08
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-06 11:27:54 +0300

    trying to fix data fetch

--- src/components/ExportExcelComponent.vue
+++ src/components/ExportExcelComponent.vue
@@ -8,20 +8,53 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, onMounted, ref } from "vue";
 import * as XLSX from "xlsx";
 
 const props = defineProps(["rows", "columns"]);
 
+console.error(props.columns);
+console.error(props.rows);
+
+let columnsWithoutLinks = ref(
+  props.columns.filter((column) => !column.name.includes("pl"))
+);
+let rowsWithoutLinks = ref(
+  props.rows.map((row) => {
+    for (let key in row) {
+      if (key.includes("pl")) {
+        delete row[key];
+      }
+    }
+    return row;
+  })
+);
+
+let columnsWithLinks = ref(
+  props.columns.filter((column) => column.name.includes("pl"))
+);
+let rowsWithLinks = ref(
+  props.rows.map((row) => {
+    for (let key in row) {
+      if (!key.includes("pl")) {
+        delete row[key];
+      }
+    }
+    return row;
+  })
+);
+console.error(props.rows);
+console.error(rowsWithLinks.value);
+
 function exportTable() {
-  let columnsSlice = props.columns.map((column) => column.name);
-  let rowsSlice = props.rows.map((row) => Object.values(row));
+  let columnsSlice = columnsWithoutLinks.value.map((column) => column.name); // get array of column names
+  let rowsSlice = rowsWithoutLinks.value.map((row) => Object.values(row)); // get array of arrays (each subarray is the row in excel)
 
-  let aoa = rowsSlice.slice();
-  aoa.unshift(columnsSlice);
+  let aoa = rowsSlice.slice(); // copy the rowsSlice
+  aoa.unshift(columnsSlice); // insert row of column names at the beginning
 
-  console.log("hey!");
-  console.warn(aoa);
+  // console.log("hey!");
+  // console.warn(aoa);
 
   const workbook = XLSX.utils.book_new();
   let worksheet = XLSX.utils.aoa_to_sheet(aoa);
