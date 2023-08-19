b824766a7549512025853e38e104d0ba30e4905f
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-19 15:23:16 +0300

    Column width is set based on max length

--- src/components/ExportExcelComponent.vue
+++ src/components/ExportExcelComponent.vue
@@ -13,11 +13,57 @@ import * as XLSX from "xlsx";
 
 const props = defineProps(["rows", "columns", "visibleColumns"]);
 
-function getMaxColumnWidthByColumnName(rows, columnName, defaultValue = 10) {
-  return rows.reduce(
-    (w, r) => Math.max(w, String(r[columnName]).length, columnName.length),
-    defaultValue
-  );
+// function getMaxColumnWidthByColumnName(rows, columnName, defaultValue = 10) {
+//   return rows.reduce(
+//     (w, r) => Math.max(w, String(r[columnName]).length, columnName.length),
+//     defaultValue
+//   );
+// }
+
+function getMaxColumnWidthByColumnName(
+  worksheet,
+  columnName,
+  startRow,
+  endRow
+) {
+  let widths = [];
+  for (let i = startRow; i < endRow; ++i) {
+    let cell = worksheet[`${columnName}${i}`]; // access current cell
+    console.log(cell);
+    if (cell === undefined) {
+      console.log(`Row ${i}`);
+      continue;
+    }
+    widths.push(String(cell.v).length); // string conversion is required because we might need to find the length of a number or the length of some other type
+  }
+  console.log("Now at " + columnName);
+  console.log(Math.max(...widths));
+  return Math.max(...widths);
+}
+
+function getColumnStyles(worksheet) {
+  const range = worksheet["!fullref"]; // get the cell range in the table (e.g. it can be A1:BC75)
+  const [startAddress, endAddress] = range.match(/[a-z]+\d+/gi); // from comment above 'startAddress' may contain A1, 'endAddress' - BC75
+  const [startColumn, endColumn] = range.match(/[a-z]+/gi); // from comment above 'startColumn' may contain A, 'endColumn' - BC
+  const [startRow, endRow] = range.match(/\d+/gi); // from comment above 'startRow' may contain 1, 'endRow' - 75
+
+  let columnStyles = []; // stores styles for each column
+
+  const startColumnDecoded = XLSX.utils.decode_col(startColumn);
+  const endColumnDecoded = XLSX.utils.decode_col(endColumn);
+
+  for (let i = startColumnDecoded; i <= endColumnDecoded; ++i) {
+    const columnName = XLSX.utils.encode_col(i); // from '0-format' to 'A1' format
+    columnStyles.push({
+      wch: getMaxColumnWidthByColumnName(
+        worksheet,
+        columnName,
+        startRow,
+        endRow
+      ),
+    });
+  }
+  return columnStyles;
 }
 
 function exportTable() {
@@ -30,6 +76,14 @@ function exportTable() {
   console.log(worksheet["A1"]);
   console.log(worksheet["A2"]);
 
+  let range = worksheet["!fullref"];
+
+  console.log(range);
+  console.log(XLSX.utils.decode_range(range));
+  console.log(XLSX.utils.encode_col(26));
+  console.log(XLSX.utils.decode_col("B"));
+
+  worksheet["!cols"] = getColumnStyles(worksheet);
   XLSX.writeFile(workbook, "test1.xlsx");
 }
 
