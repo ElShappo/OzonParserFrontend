54d22e2574a931760e7c076c8d3c63ced3bcf38b
Deni Shaipov <deni.shaipp@gmail.com>
2023-08-20 19:21:27 +0300

    added ExcelJS library to add styling to excel tables (like stylink links, aligning items, freezing rows and cols)

--- src/components/ExportExcelComponent.vue
+++ src/components/ExportExcelComponent.vue
@@ -10,77 +10,64 @@
 <script setup>
 import { defineProps } from "vue";
 import * as XLSX from "xlsx";
+import Excel from "exceljs/dist/exceljs";
+const FileSaver = require("file-saver");
 
 const props = defineProps(["rows", "columns", "visibleColumns"]);
 
-function getMaxColumnWidthByColumnName(
-  worksheet,
-  columnName,
-  startRow,
-  endRow,
-  method
-) {
-  let widths = [];
-  for (let i = startRow; i < endRow; ++i) {
-    let cell = worksheet[`${columnName}${i}`]; // access current cell
-    console.log(cell);
-    if (cell === undefined) {
-      console.log(`Row ${i}`);
-      continue;
-    }
-    widths.push(String(cell.v).length); // string conversion is required because we might need to find the length of a number or the length of some other type
-  }
-  console.log("Now at " + columnName);
-  console.log(method(widths));
-  return method(widths);
-}
-
-function getColumnStyles(worksheet, method) {
-  const range = worksheet["!fullref"]; // get the cell range in the table (e.g. it can be A1:BC75)
-  const [startAddress, endAddress] = range.match(/[a-z]+\d+/gi); // from comment above 'startAddress' may contain A1, 'endAddress' - BC75
-  const [startColumn, endColumn] = range.match(/[a-z]+/gi); // from comment above 'startColumn' may contain A, 'endColumn' - BC
-  const [startRow, endRow] = range.match(/\d+/gi); // from comment above 'startRow' may contain 1, 'endRow' - 75
-
-  let columnStyles = []; // stores styles for each column
-
-  const startColumnDecoded = XLSX.utils.decode_col(startColumn);
-  const endColumnDecoded = XLSX.utils.decode_col(endColumn);
-
-  for (let i = startColumnDecoded; i <= endColumnDecoded; ++i) {
-    const columnName = XLSX.utils.encode_col(i); // from '0-format' to 'A1' format
-    columnStyles.push({
-      wch: getMaxColumnWidthByColumnName(
-        worksheet,
-        columnName,
-        startRow,
-        endRow,
-        method
-      ),
-    });
-  }
-  return columnStyles;
-}
-
-function exportTable() {
+async function exportTable() {
   let table = document.querySelector("#main-table table");
   const workbook = XLSX.utils.table_to_book(table);
-  let worksheet = workbook.Sheets["Sheet1"];
+  const buffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
 
-  console.log(workbook.SheetNames);
-  console.log(worksheet);
-  console.log(worksheet["A1"]);
-  console.log(worksheet["A2"]);
+  const wb = new Excel.Workbook();
+  await wb.xlsx.load(buffer);
+  const ws = wb.getWorksheet("Sheet1");
 
-  let range = worksheet["!fullref"];
-
-  console.log(range);
-  console.log(XLSX.utils.decode_range(range));
-  console.log(XLSX.utils.encode_col(26));
-  console.log(XLSX.utils.decode_col("B"));
-
-  worksheet["!cols"] = getColumnStyles(worksheet, function (widths) {
-    return Math.max(...widths);
+  const newWb = new Excel.Workbook();
+  const newWs = newWb.addWorksheet("Sheet1", {
+    views: [{ state: "frozen", xSplit: 2, ySplit: 3 }],
   });
-  XLSX.writeFile(workbook, "test1.xlsx");
+
+  ws.eachRow(function (row, i) {
+    newWs.addRow(row.values);
+  });
+
+  newWs.eachRow(function (row, rowNumber) {
+    row.alignment = { vertical: "middle", horizontal: "center" };
+  });
+
+  const autoWidth = (worksheet, minimalWidth = 5) => {
+    worksheet.columns.forEach((column) => {
+      let maxColumnLength = 0;
+      column.eachCell({ includeEmpty: true }, (cell) => {
+        if (typeof cell.value === "object") {
+          const linkStyle = {
+            underline: true,
+            color: { argb: "FF0000FF" },
+          };
+          cell.font = linkStyle;
+        }
+        maxColumnLength = Math.max(
+          maxColumnLength,
+          minimalWidth,
+          cell.value
+            ? typeof cell.value === "object"
+              ? cell.value.text.toString().length
+              : cell.value.toString().length
+            : 0
+        );
+      });
+      column.width = maxColumnLength + 2;
+    });
+  };
+
+  autoWidth(newWs, 5);
+
+  newWs.mergeCells("A1:A3");
+  newWs.mergeCells("B1:B3");
+
+  const newBuffer = await newWb.xlsx.writeBuffer();
+  FileSaver.saveAs(new Blob([newBuffer]), "result.xlsx");
 }
 </script>
